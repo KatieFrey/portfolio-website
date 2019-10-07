fc674cdfa6a9f49f9468bf64d9df9129daa1033d
Katharine Francis <katie-francis@hotmail.com>
2019-10-07 13:54:15 -0400

    Fetching Batman data

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const BlogLink = props => {
-  return (
-    <li>
-      <Link href="/p/[id]" as={`/p/${props.id}`}>
-        <a>{props.id}</a>
-      </Link>
-    </li>
-  );
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
 };
 
-export default () => {
-  return (
-    <Layout>
-      <p>Hey guys!</p>
-      <h1>New Blogs</h1>
-      <ul>
-        <BlogLink id="necklaces-earrings" />
-        <BlogLink id="notebooks-pens" />
-        <BlogLink id="gym-shoes" />
-      </ul>
-    </Layout>
-  );
-};
+export default Index;
