52c17facc50fc23bbb5266a3afaeb4a500ccf737
Katharine Francis <katie-francis@hotmail.com>
2020-04-27 21:42:56 -0700

    Fixed the parameters in dynamic routing and the external links

--- data/projectData.js
+++ data/projectData.js
@@ -5,8 +5,10 @@ export const projectData = {
     tech: ["React", "Redux", "Node.js/Express", "Postgres", "Passport.js"],
     description:
       "Red Tracker is a group project created to help administrators write their own applications for future candidates.",
-    githubLink: ["https://github.com/inclusion-red"],
-    website: "https://red-applicant-tracker.herokuapp.com",
+    githubLink: "//github.com/inclusion-red",
+    githubTitle: "github.com/inclusion-red",
+    website: "//red-applicant-tracker.herokuapp.com",
+    websiteTitle: "red-applicant-tracker.herokuapp.com",
   },
   2: {
     img: "/stock_portfolio_card.png",
@@ -14,8 +16,10 @@ export const projectData = {
     tech: ["React", "Redux", "Reactstrap", "IEX-Cloud-API", "Firebase"],
     description:
       "My Stock Portfolio is a calculating tool that shows the total amount you would have to spend for a number of shares for a particular stock.",
-    githubLink: ["https://github.com/KatieFrey/stockPortfolio"],
-    website: "https://stock-portfolio-iex-7jv6ufya5.now.sh/",
+    githubLink: "//github.com/KatieFrey/stockPortfolio",
+    githubTitle: "github.com/KatieFrey/stockPortfolio",
+    website: "//stock-portfolio-iex-7jv6ufya5.now.sh/",
+    websiteTitle: "stock-portfolio-iex-7jv6ufya5.now.sh/",
   },
   3: {
     img: "/butterflies_card.png",
@@ -30,10 +34,9 @@ export const projectData = {
     ],
     description:
       "Support Butterflies was created during the Covid-19 epidemic. It was made to show support towards healthcare workers and medical professionals on the frontline. General users can go to the website and press a butterfly button to express their gratitude and the floating butterfly can be seen in realtime from any browser.",
-    githubLink: [
-      "https://github.com/karuna24s/butterfly-client",
-      "https://github.com/karuna24s/butterfly-server",
-    ],
-    website: "https://butterflies.herokuapp.com/",
+    githubLink: "//github.com/Support-Butterflies",
+    githubTitle: "github.com/Support-Butterflies",
+    website: "//butterflies.herokuapp.com",
+    websiteTitle: "butterflies.herokuapp.com",
   },
 };
